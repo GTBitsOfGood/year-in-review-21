45878c4bc9f8853053ca706158a2e78a632a3a1b
Ila Sharma <isharma37@gatech.edu>
2021-04-06 18:24:13 -0400

    added to index.css

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import recruitmentscreen from "./components/recruitmentscreen";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          DESIGN
-          DESIGN
-          DESIGN
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <recruitmentscreen/>
+    </section>
   );
 }
 
---
+++ src/components/recruitmentscreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const recruitmentscreen = () => (
+        <section>
+            <div id = "recruitment">
+            </div>
+        </section>
+
+)
+
+export default recruitmentscreen
