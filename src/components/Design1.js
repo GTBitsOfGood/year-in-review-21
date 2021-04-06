ec9e336a43c897dc3265490a1ed4efb952ea673d
ndasari6 <ndasari6@gatech.edu>
2021-04-06 17:00:06 -0400

    setting background & design titles

---
+++ src/components/Design1.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Design1 = () => (
+        <section>
+            <div id="back">
+                <image id="pants"></image>
+            </div>
+            <div id="dTitle1" class="dTitle">DESIGN</div>
+            <div id="dTitle2" class="dTitle">DESIGN</div>
+            <div id="dTitle3" class="dTitle">DESIGN</div>
+        </section>
+
+)
+
+
+export default Design1
